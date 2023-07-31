436c89b957e121f10a95d6a9498f0863365b45fe
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-31 18:35:33 +0300

    Some fixes for demo script

--- demo.py
+++ demo.py
@@ -2,23 +2,20 @@ from teneva_bm import *
 from teneva_opti import *
 
 
-def demo(d=100, steps=250, m=1.E+2):
+def demo(d=100, steps=250, m=1.E+4):
     bm = BmQuboKnapDet(d)
 
     opti = OptiTensTtopt(bm, m, log_info=True, log_file=True)
     opti.opts(rank=4)
     opti.run()
+    opti.save()
 
 
     print('\n\n')
 
-    # opti.save()
-    # data = opti.load()
-    # print(data['history']['bm']['y_list'])
-    # return
-
     opti = OptiTensProtes(bm, m, log_info=True, log_file=True)
     opti.run()
+    opti.save()
 
     print('\n\n')
 
@@ -29,6 +26,7 @@ def demo(d=100, steps=250, m=1.E+2):
     opti.run()
     opti.render()
     opti.show()
+    opti.save()
 
     print('\n\n')
 
@@ -36,6 +34,15 @@ def demo(d=100, steps=250, m=1.E+2):
     opti.run()
     opti.render()
     opti.show()
+    opti.save()
+
+    print('\n\n')
+
+    print('Demo for result loading (we present y_list below):')
+    data = opti.load()
+    print(data['history']['bm']['y_list'][:25])
+    print('...')
+    print(data['history']['bm']['y_list'][-25:])
 
 
 if __name__ == '__main__':
