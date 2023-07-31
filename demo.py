cf2530e650cba58c334c017cba2a8787498f5108
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-31 18:16:05 +0300

    More accurate demo script

--- demo.py
+++ demo.py
@@ -2,15 +2,34 @@ from teneva_bm import *
 from teneva_opti import *
 
 
-def demo(d=100, m=1.E+4):
+def demo(d=100, steps=250, m=1.E+2):
     bm = BmQuboKnapDet(d)
+
     opti = OptiTensTtopt(bm, m, log_info=True, log_file=True)
     opti.opts(rank=4)
     opti.run()
 
-    bm = BmQuboKnapDet(d)
+    print('\n\n')
+
     opti = OptiTensProtes(bm, m, log_info=True, log_file=True)
     opti.run()
+
+    print('\n\n')
+
+    bm = BmAgentSwimmer(steps=steps)
+
+    opti = OptiTensTtopt(bm, m, log_info=True, log_file=True)
+    opti.opts(rank=4)
+    opti.run()
+    opti.render()
+    opti.show()
+
+    print('\n\n')
+
+    opti = OptiTensProtes(bm, m, log_info=True, log_file=True)
+    opti.run()
+    opti.render()
+    opti.show()
 
 
 if __name__ == '__main__':
