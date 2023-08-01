d4613566944f8fed2279a3a6f5b94bc4f8343ad0
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-01 19:16:43 +0300

    Update OptiTensPortfolio

--- teneva_opti/tens/opti_tens_portfolio.py
+++ teneva_opti/tens/opti_tens_portfolio.py
@@ -4,9 +4,15 @@ import nevergrad as ng
 from teneva_opti import OptiTens
 
 
+DESC = """
+    The Portfolio optimizer. See the repo nevergrad:
+    https://github.com/facebookresearch/nevergrad
+"""
+
+
 class OptiTensPortfolio(OptiTens):
     def __init__(self, *args, **kwargs):
-        super().__init__('portfolio', *args, **kwargs)
+        super().__init__('portfolio', DESC, *args, **kwargs)
 
     def _optimize(self):
-        self._optimize_ng(ng.optimizers.Portfolio)
+        self._optimize_ng_helper(ng.optimizers.Portfolio)
