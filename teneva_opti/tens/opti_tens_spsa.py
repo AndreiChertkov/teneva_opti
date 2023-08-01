5ee2bc7781686f6752f2b7e0580a82388a15020d
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-01 19:16:29 +0300

    Update OptiTensSpsa

--- teneva_opti/tens/opti_tens_spsa.py
+++ teneva_opti/tens/opti_tens_spsa.py
@@ -4,9 +4,15 @@ import nevergrad as ng
 from teneva_opti import OptiTens
 
 
+DESC = """
+    The SPSA optimizer. See the repo nevergrad:
+    https://github.com/facebookresearch/nevergrad
+"""
+
+
 class OptiTensSpsa(OptiTens):
     def __init__(self, *args, **kwargs):
-        super().__init__('spsa', *args, **kwargs)
+        super().__init__('spsa', DESC, *args, **kwargs)
 
     def _optimize(self):
-        self._optimize_ng(ng.optimizers.SPSA)
+        self._optimize_ng_helper(ng.optimizers.SPSA)
