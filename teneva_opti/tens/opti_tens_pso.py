71e4453c3885ed0dfb84157fbf0040d0408f444d
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-01 19:16:35 +0300

    Update OptiTensPso

--- teneva_opti/tens/opti_tens_pso.py
+++ teneva_opti/tens/opti_tens_pso.py
@@ -4,9 +4,15 @@ import nevergrad as ng
 from teneva_opti import OptiTens
 
 
+DESC = """
+    The PSO optimizer. See the repo nevergrad:
+    https://github.com/facebookresearch/nevergrad
+"""
+
+
 class OptiTensPso(OptiTens):
     def __init__(self, *args, **kwargs):
-        super().__init__('pso', *args, **kwargs)
+        super().__init__('pso', DESC, *args, **kwargs)
 
     def _optimize(self):
-        self._optimize_ng(ng.optimizers.PSO)
+        self._optimize_ng_helper(ng.optimizers.PSO)
