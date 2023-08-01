f75fb2c35695aea8424d81fe55c8aa69094fda4c
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-01 19:16:57 +0300

    Update OptiTensOpo

--- teneva_opti/tens/opti_tens_opo.py
+++ teneva_opti/tens/opti_tens_opo.py
@@ -4,9 +4,15 @@ import nevergrad as ng
 from teneva_opti import OptiTens
 
 
+DESC = """
+    The OnePlusOne optimizer. See the repo nevergrad:
+    https://github.com/facebookresearch/nevergrad
+"""
+
+
 class OptiTensOpo(OptiTens):
     def __init__(self, *args, **kwargs):
-        super().__init__('opo', *args, **kwargs)
+        super().__init__('opo', DESC, *args, **kwargs)
 
     def _optimize(self):
-        self._optimize_ng(ng.optimizers.OnePlusOne)
+        self._optimize_ng_helper(ng.optimizers.OnePlusOne)
