f0e0424100e34c37ba0891d15d437ebdfce83981
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-01 19:17:16 +0300

    Update OptiTensNb

--- teneva_opti/tens/opti_tens_nb.py
+++ teneva_opti/tens/opti_tens_nb.py
@@ -4,9 +4,15 @@ import nevergrad as ng
 from teneva_opti import OptiTens
 
 
+DESC = """
+    The NoisyBandit optimizer. See the repo nevergrad:
+    https://github.com/facebookresearch/nevergrad
+"""
+
+
 class OptiTensNb(OptiTens):
     def __init__(self, *args, **kwargs):
-        super().__init__('nb', *args, **kwargs)
+        super().__init__('nb', DESC, *args, **kwargs)
 
     def _optimize(self):
-        self._optimize_ng(ng.optimizers.NoisyBandit)
+        self._optimize_ng_helper(ng.optimizers.NoisyBandit)
