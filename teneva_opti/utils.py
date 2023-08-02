b4c164142e10637fb171990dbacb01178dcffbad
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-02 16:47:43 +0300

    More functions in utils.py

--- teneva_opti/utils.py
+++ teneva_opti/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 
 
@@ -14,7 +15,7 @@ class Log:
         if self.with_log:
             print(text)
         if self.fpath:
-            opt = 'w' if self.is_file_new and not self.is_file_add else 'a'
+            opt = 'w' if self.is_file_new and not self.is_file_add else 'a+'
             with open(self.fpath, opt) as f:
                 f.write(text + '\n')
             self.is_file_new = False
@@ -34,6 +35,47 @@ class Log:
 
     def wrn(self, content=''):
         self(f'WRN ! {content}')
+
+
+def get_identity(obj):
+    res = {}
+
+    for id in obj.identity:
+        v = getattr(obj, id)
+
+        if v is None:
+            msg = f'Identity "{id}" is not set'
+            raise ValueError(msg)
+
+        if isinstance(v, (list, np.ndarray)):
+            if len(v) == 0:
+                msg = f'List for identity "{id}" is empty'
+                raise ValueError(msg)
+            if isinstance(v[0], float):
+                msg = 'Float identity is not supported'
+                raise NotImplementedError(msg)
+            if len(set(v)) > 1:
+                msg = 'List-like identity is not supported'
+                raise NotImplementedError(msg)
+            v = v[0]
+
+        if isinstance(v, float):
+            msg = 'Float identity is not supported'
+            raise NotImplementedError(msg)
+
+        res[id] = v
+
+    return res
+
+
+def get_identity_str(obj, pretty=False):
+    res = []
+    for id, v in get_identity(obj).items():
+        if isinstance(v, bool):
+            res.append(f'{id}')
+        else:
+            res.append(f'{id}: {v}' if pretty else f'{id}-{v}')
+    return ('; ' if pretty else '__').join(res)
 
 
 def path(fpath=None, ext=None):
