ba8e17e2cbc4bb2c6b7d6909669b7604d247faa4
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-02 18:59:13 +0300

    Demo for OptiManager

--- demo_baseline.py
+++ demo_baseline.py
@@ -1,23 +1,18 @@
 from teneva_bm import *
 from teneva_opti import *
 
-M = 1.E+2
-STEPS = 250
 
-BMS = [
-    BmQuboKnapDet(d=10),
-    BmQuboKnapDet(d=20),
-    BmQuboKnapDet(d=50),
-    BmQuboKnapDet(d=80),
-    BmQuboKnapDet(d=100),
-    BmAgentAnt(steps=STEPS),
-    BmAgentHuman(steps=STEPS),
-    BmAgentHumanStand(steps=STEPS),
-    BmAgentLake(steps=STEPS),
-    BmAgentPendInv(steps=STEPS),
-    BmAgentPendInvDouble(steps=STEPS),
-    BmAgentSwimmer(steps=STEPS),
-]
+TASKS = []
+
+
+BM_M = 1.E+2
+
+
+BM_AGENT_STEPS = 250
+BM_AGENT_M = 1.E+2
+
+
+BM_OPTI_SEEDS = [0, 1]
 
 
 OPTIS = [
@@ -29,11 +24,45 @@ OPTIS = [
     OptiTensNb,
     OptiTensSpsa,
     OptiTensPortfolio,
+][:3]
+
+
+BMS_AGENT = [
+    BmAgentAnt,
+    BmAgentHuman,
+    BmAgentHumanStand,
+    BmAgentPendInv,
+    BmAgentPendInvDouble,
+    BmAgentSwimmer,
 ]
 
 
+SEEDS = [0, 1]
+
+
+for d in [10, 20, 50, 80, 100]:
+    for Opti in OPTIS:
+        for seed in SEEDS:
+            TASKS.append({
+                'bm': BmQuboKnapDet,
+                'bm_args': {'d': d},
+                'opti': Opti,
+                'opti_args': {'m': BM_M, 'seed': seed},
+            })
+
+
+for Bm in BMS_AGENT:
+    for Opti in OPTIS:
+        TASKS.append({
+            'bm': Bm,
+            'bm_args': {'steps': BM_AGENT_STEPS},
+            'opti': Opti,
+            'opti_args': {'m': BM_AGENT_M},
+        })
+
+
 def demo_baseline():
-    oman = OptiManager(BMS, OPTIS, M, fold='result_baseline')
+    oman = OptiManager(TASKS, fold='result_demo_baseline')
     oman.run()
 
 
