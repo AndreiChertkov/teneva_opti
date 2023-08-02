797399843480f73432c9daeccf5947e40d3d9345
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-02 18:58:41 +0300

    More accurate nevergrad usage

--- teneva_opti/opti_tens.py
+++ teneva_opti/opti_tens.py
@@ -11,18 +11,24 @@ class OptiTens(Opti):
         if not self.is_n_equal:
             raise NotImplementedError
 
-        optimizer = solver(
-            parametrization=ng.p.TransitionChoice(range(self.n0),
-            repetitions=self.d),
-            budget=1.E+99,
-            num_workers=1)
+        parametrization = ng.p.TransitionChoice(
+            range(self.n0), repetitions=self.d)
+        parametrization.random_state.seed(self.seed)
 
-        recommendation = optimizer.provide_recommendation()
+        optimizer = solver(parametrization=parametrization,
+            budget=None, num_workers=1)
 
         while True:
             x = optimizer.ask()
             i = np.array(x.value, dtype=int)
             y = self.target(i)
-            if y is None:
+            if y is None or self.bm.m == self.bm.budget_m-1:
                 break
-            optimizer.tell(x, y)
+            optimizer.tell(x, -y if self.is_max else y)
+
+        # We call for the final recommendation:
+        x = optimizer.provide_recommendation()
+        i = np.array(x.value, dtype=int)
+        for _ in range(2):
+            # We repeat it to stop the Bm
+            y = self.bm.get(i, skip_cache=True)
