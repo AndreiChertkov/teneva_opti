4d756bc1ce5c0cba6a475961322a2595b834de99
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-04 15:36:30 +0300

    Update demo for OptiManager

--- demo_baseline.py
+++ demo_baseline.py
@@ -62,9 +62,25 @@ for Bm in BMS_AGENT:
 
 
 def demo_baseline():
-    oman = OptiManager(TASKS, fold='result_demo_baseline')
-    oman.run()
+    #oman = OptiManager(TASKS, fold='result_demo_baseline')
+    #oman.run()
 
+    oman = OptiManager(fold='result_demo_baseline', is_show=True)
+    oman.load()
+    oman.filter(d=100, bm_name='QuboKnapDet')
+    oman.sort_by_op(['protes', 'ttopt'])
+    oman.join_op_seed()
+
+    print('\n\nLoaded result for QuboKnapDet:\n')
+    oman.show_text()
+
+    oman.show_table('\n\nTable for mean:')
+    oman.show_table('\n\nTable for best:', kind='best')
+    oman.show_table('\n\nTable for wrst:', kind='wrst')
+
+    oman.show_table('\n\nTable for mean time:', prec=1, is_time=True)
+    oman.show_table('\n\nTable for best time:', kind='best', is_time=True)
+    oman.show_table('\n\nTable for wrst time:', kind='wrst', is_time=True)
 
 if __name__ == '__main__':
     demo_baseline()
